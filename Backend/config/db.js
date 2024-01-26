26fbd807fda6be85a2aab2439abbfe36f2335d8c
Ayush_God <ayushgodbole31@gmail.com>
2024-01-26 15:22:56 +0530

    LMS Mern project

---
+++ Backend/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// if user gives extra information , ignore it
+mongoose.set('strictQuery',false);
+
+const connectToDb = async()=>{
+    try {
+        const {connection} = await mongoose.connect(
+            process.env.MONGOURL
+        );
+    
+        // consoling
+        if(connection){
+            console.log(`Connected to mongo db as host ${connection.host}`)
+        }
+    } catch (error) {
+       console.log(`Error in connecting to DB`);
+       process.exit(1); 
+    }
+}
+
+export default connectToDb;
